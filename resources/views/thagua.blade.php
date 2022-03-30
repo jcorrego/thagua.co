a274ae90aafaf835122047ec91e032cef2aab934
Juan Carlos Orrego <jcorrego@gmail.com>
2022-03-30 15:06:23 -0500

    Add favicon

--- resources/views/thagua.blade.php
+++ resources/views/thagua.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Thagua</title>
+        <link rel="shortcut icon" href="/favicon.ico">
         <link href="/css/app.css" rel="stylesheet">
     </head>
     <body class="bg-slate-400">
