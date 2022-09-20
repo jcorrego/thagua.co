051907d7f342e70f48fd13835ba1aeb3aae6215a
Juan Carlos Orrego <jcorrego@gmail.com>
2022-09-20 09:33:56 -0500

    Update thagua.blade.php

--- resources/views/thagua.blade.php
+++ resources/views/thagua.blade.php
@@ -8,7 +8,7 @@
         <link href="/css/app.css" rel="stylesheet">
     </head>
     <body style="background-color: rgb(131,131,131);">
-        <div class="relative flex justify-center min-h-screen bg-slate-400 items-center py-4">
+        <div class="relative flex justify-center min-h-screen items-center py-4">
             <div class="max-w-xl mx-auto w-2/3">
                 <img src="images/web-logo-thagua.svg" class="w-48 mx-auto" alt="Thagua.co">
                 <p class="mt-10 text-center text-white text-lg">"We offer you an innovative experience with modern and functional products that make your daily life easier."</p>
