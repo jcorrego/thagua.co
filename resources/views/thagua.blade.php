78a7f167de893f1272dee00e4643e3dbdf9dd4f6
Juan Carlos Orrego <jcorrego@gmail.com>
2022-09-20 09:32:26 -0500

    Update thagua.blade.php

--- resources/views/thagua.blade.php
+++ resources/views/thagua.blade.php
@@ -7,10 +7,10 @@
         <link rel="shortcut icon" href="/favicon.ico">
         <link href="/css/app.css" rel="stylesheet">
     </head>
-    <body class="bg-slate-400">
+    <body style="background-color: rgb (131 131 131);">
         <div class="relative flex justify-center min-h-screen bg-slate-400 items-center py-4">
             <div class="max-w-xl mx-auto w-2/3">
-                <img src="images/logo_thagua.svg" class="w-48 mx-auto" alt="Thagua.co">
+                <img src="images/web-logo-thagua.svg" class="w-48 mx-auto" alt="Thagua.co">
                 <p class="mt-10 text-center text-white text-lg">"We offer you an innovative experience with modern and functional products that make your daily life easier."</p>
                 <h2 class="mt-10 text-center text-white text-base">Contact information</h2>
                 <p class=" text-center text-white opacity-70">
